a4e8d5674b7983db0873d8420dc4dabea4f32b15
platypus-perry03 <choe31904@gmail.com>
2025-10-30 14:59:35 +0900

    Updated Changes

--- lib/week01/test2.dart
+++ lib/week01/test2.dart
@@ -1,17 +1,11 @@
 import 'package:flutter/fluter.dart';
+import 'package:flutter/'
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: GreetingPage(),
-    );
-  }
+  const MyApp
 }
 
