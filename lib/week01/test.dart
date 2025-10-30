1c424e6e6292f5c810522f28c84b90b52e43a64d
platypus-perry03 <choe31904@gmail.com>
2025-10-30 14:53:05 +0900

    Updated Changes

---
+++ lib/week01/test.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: GreetingPage(),
+    );
+  }
+}
+
+class GreetingPage extends StatefulWidget {
+  const GreetingPage({super.key});
+
+  @override
+  State<GreetingPage> createState() => _GreetingPageState();
+}
+
+class _GreetingPageState extends State<GreetingPage> {
+  String message = '안녕하세요'; // 초기 상태
+
+  void changeMessage() {
+    setState(() {
+      // 상태 변경
+      message = (message == '안녕하세요') ? '반가워요' : '안녕하세요';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('상태 변경 예제'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          message,
+          style: const TextStyle(fontSize: 30),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: changeMessage,
+        child: const Icon(Icons.refresh),
+      ),
+    );
+  }
+}
