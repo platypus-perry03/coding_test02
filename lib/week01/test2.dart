a9dd811fb0f05f1be891a65fd1666e29053aef70
platypus-perry03 <choe31904@gmail.com>
2025-10-30 15:07:35 +0900

    Updated Changes

--- lib/week01/test2.dart
+++ lib/week01/test2.dart
@@ -4,4 +4,11 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
