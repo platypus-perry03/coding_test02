63fc4d6eff37fae475f70beea56b17cfd25083cb
platypus-perry03 <choe31904@gmail.com>
2025-10-30 15:07:05 +0900

    Updated Changes

--- lib/week01/test2.dart
+++ lib/week01/test2.dart
@@ -4,42 +4,4 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: GreetingPage(),
-    );
-  }
-}
-
-class GreetingPage extends StatefulWidget {
-  const GreetingPage({super.key});
-
-  @override
-  State<GreetingPage> createState() => _GreetingPageState();
-}
-
-class _GreetingPageState extends State<GreetingPage> {
-  String message = '안녕하세요';
-
-  void changeMessage() {
-    setState(() {
-      // 상태 변경
-      message = (message == '안녕하세요') ? '반가워요' : '안녕하세요';
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('상태 변경 예제'),
-        centerTitle: true,
-      ),
-    ),
-  }
-  
-}
+class MyApp extends StatelessWidget
